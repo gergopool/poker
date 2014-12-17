3248858a594de2135b3d4a1dcbe93c3a7d2ee35d
Gergely Papp <gergopool@gmail.com>
2014-12-17 09:10:24 +0100

    Fold fault fixed.

--- src/Table.java
+++ src/Table.java
@@ -148,7 +148,7 @@ public class Table
 
       determineNextPlayerID();
 
-    } while(lastRaiserID != currentPlayerID);
+    } while(lastRaiserID != currentPlayerID && !endOfRound());
 
     moveCoinsToPot();
   }
